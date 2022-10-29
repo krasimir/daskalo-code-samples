8c0028076c7a1e79d346d5a5197375f87e827752
Krasimir <krasimir.st.tsonev@gmail.com>
2022-10-29 19:01:03 +0300

    JavaScript variables

---
+++ samples/js-vars-basics/script.js
@@ -0,0 +1,19 @@
+var country;
+var age = 37; // number
+var name = `Krasimir
+Tsonev
+`; // string
+var isRegistered = false; // boolean (true, false)
+var job; // undefined
+
+if (isRegistered) {
+  country = "Bulgaria";
+  console.log(name);
+} else {
+  console.log("User is not registered");
+}
+
+console.log(country);
+
+var a = 20, b = 40, c;
+var firstName = "Krasimir"
