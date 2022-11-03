d8e1e865bdb5f2a99c4b48c7f8ddcc75abc218d5
Krasimir <krasimir.st.tsonev@gmail.com>
2022-11-03 22:41:08 +0200

    Operators

---
+++ samples/js-operators/script.js
@@ -0,0 +1,12 @@
+var age1 = 45;
+var age2 = '30';
+var age3 = 45;
+
+var isBigger = age1 < age3;
+
+var isRaining = false;
+var isCloudy = false;
+var result = !isRaining && !isCloudy;
+
+console.log(isRaining, isCloudy);
+console.log(result);
