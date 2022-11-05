c5647756b5009ac321700eb7f583353f83e10363
Krasimir <krasimir.st.tsonev@gmail.com>
2022-11-05 17:48:14 +0200

    JavaScript objects

---
+++ samples/js-arrays-objects-set-map/script.js
@@ -0,0 +1,11 @@
+var user = {
+  name: "krasimir",
+  age: 37,
+  confirmed: true,
+  message: function () {
+    console.log('Hello world.')
+  }
+}
+
+console.log("Text here".toUpperCase());
+console.log(window.innerWidth);
