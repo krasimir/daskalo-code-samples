54740716ce328217fdb29711c6aaaa62eded1d15
Krasimir <krasimir.st.tsonev@gmail.com>
2022-11-10 16:22:18 +0200

    Arrays

---
+++ samples/js-arrays/script.js
@@ -0,0 +1,13 @@
+var name = "Krasimir"; // string
+var age = 30; // number;
+var isRegistered = false; // boolean
+
+var temp = [ 25, 26, 21, 30, 32, 33, 29 ];
+
+temp.push("test here")
+
+console.log(temp);
+
+temp[0] = ['A', 'B', 'C']
+
+console.log(temp);
