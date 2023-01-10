f78198451ca2cb3ca146709c6d7e980f08f81234
Krasimir <krasimir.st.tsonev@gmail.com>
2023-01-10 09:39:14 +0200

    Revert some changes

--- do-you-speak-js/iterable-protocol/code.js
+++ do-you-speak-js/iterable-protocol/code.js
@@ -1,29 +1,5 @@
 const obj = {
   name: 'Bob',
   skills: ['HTML', 'CSS', 'JavaScript'],
-  [Symbol.iterator]: function () {
-    const skills = this.skills;
-    let i = 0;
-    return {
-      next() {
-        if (i >= skills.length) {
-          return {
-            done: true
-          }  
-        }
-        return {
-          done: false,
-          value: skills[i++]
-        }
-      }
-    }
-  }
 }
 const arr = ['a', 'b', 'c', 'd'];
-const iterator = arr[Symbol.iterator]();
-
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
