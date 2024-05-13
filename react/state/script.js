8df2d5a1ed3b1da3caf3196ce0bf8f3875b5c702
Krasimir <krasimir.st.tsonev@gmail.com>
2024-05-13 09:43:07 +0300

    useState + useReducer

--- react/state/script.js
+++ react/state/script.js
@@ -1,8 +1,55 @@
-const { useState } = React;
+const { useEffect, useState, useReducer } = React;
 
-function App() {
+const GET_USER_URL = 'https://jsonplaceholder.typicode.com/users';
+
+function usersReducer(users, action) {
+  let idx;
+  switch (action.type) {
+    case 'init':
+      return action.data;
+    case 'up':
+      idx = users.findIndex(user => user.id === action.id);
+      if (idx !== -1 && idx !== 0) {
+        const tmp = users[idx];
+        users[idx] = users[idx - 1];
+        users[idx - 1] = tmp;
+      }
+      return [...users];
+    case 'down':
+      idx = users.findIndex(user => user.id === action.id);
+      if (idx !== -1 && idx !== users.length - 1) {
+        const tmp = users[idx];
+        users[idx] = users[idx + 1];
+        users[idx + 1] = tmp;
+      }
+      return [...users];
+  }
+  return users;
+}
+
+function App() {  
+  const [users, updateUsers] = useReducer(usersReducer, []);
+
+  useEffect(() => {
+    fetch(GET_USER_URL)
+      .then(response => response.json())
+      .then(data => {
+        updateUsers({ type: 'init', data });
+      });
+  }, []);
+
   return (
-    <>test</>
+    <>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name}
+            <button onClick={() => updateUsers({ type: 'up', id: user.id })}>up</button>
+            <button onClick={() => updateUsers({ type: 'down', id: user.id })}>down</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
