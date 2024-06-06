880951be93bbdc52bca44764592b295b1ebf5418
Krasimir <krasimir.st.tsonev@gmail.com>
2024-06-06 18:57:21 +0300

    Styled-components

---
+++ react/styled-components/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <h1>Hello, <span>user</span></h1>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
